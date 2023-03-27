bb7ead784ef1b28a2648901ba239c6a0f603f992
Hadi <hadilearn01@gmail.com>
2023-03-27 16:40:36 +0700

    modified cta whatsapp and gallery image title animation

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -2,18 +2,30 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 const Cta = () => {
-  const phoneNumber = '+6281804095747'; // replace with the recipient's phone number
-  const message = 'Hello Dua Putri Jogja'; // replace with the message you want to send
+  const phoneNumber = [
+    '+6281804095747',
+    '+628175453045'
+  ]
+  const message = 'Hallo Dua Putri Jogja' // replace with the message you want to send
   
-  const handleWhatsAppClick = (e) => {
+  const handleWhatsAppClick1 = (e) => {
     e.preventDefault();
-    window.open(`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`, '_blank');
+    window.open(`https://api.whatsapp.com/send?phone=${phoneNumber[0]}&text=${message}`, '_blank');
+  }
+  const handleWhatsAppClick2 = (e) => {
+    e.preventDefault();
+    window.open(`https://api.whatsapp.com/send?phone=${phoneNumber[1]}&text=${message}`, '_blank');
   }
 
   return (
-    <Button href="#" onClick={handleWhatsAppClick}>
-      Open WhatsApp
+    <>
+    <Button href="#" onClick={handleWhatsAppClick1}>
+      CS1-081804095747
     </Button>
+    <Button href="#" onClick={handleWhatsAppClick2}>
+      CS2-08175453045
+    </Button>
+    </>
   );
 }
 
--- src/components/Latest.js
+++ src/components/Latest.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import image1 from '../assets/images/organik tanpa msg - Bubur Beras Organik-Salmon Wortel.jpeg'
 import image2 from '../assets/images/hero/2.png'
 import image3 from '../assets/images/hero/3.png'
@@ -21,58 +21,60 @@ const Latest = () => {
         {/* <SVG4 /> */}
       </div>
       <Container fluid>
-          <h3>Produk Terbaru</h3>
-          <p>Berbagai macam produk sehat dan higienis</p>
-          <Row className='row-cols-lg-3'>
-            <Col>
-              <img 
-                src={image1}
-                alt='Bubur Beras Organik Rasa Salmon Wortel'
-              />
-              <h5>Bubur Beras organik</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-            <Col>
-            <img 
-                src={image2}
-                alt='organik tanpa msg - Beras Organik'
-              />
-              <h5>Beras organik</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-            <Col>
-            <img 
-                src={image3}
-                alt='organik tanpa msg - Abon Tanpa MSG'
-              />
-              <h5>Abon 100% Daging Asli</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-            <Col>
-              <img 
-                src={image4}
-                alt='organik tanpa msg - Tepung Bumbu Qreezpy'
-              />
-              <h5>Tepung Bumbu Krispi Serbaguna</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-            <Col>
-            <img 
-                src={image5}
-                alt='organik tanpa msg - Kaldu Ayam'
-              />
-              <h5>Kaldu Jamur</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-            <Col>
-            <img 
-                src={image6}
-                alt='organik tanpa msg - Bumbu Instan'
-              />
-              <h5>Bumbu Instan Rempah</h5>
-              <p>Tanpa MSG, Tanpa Pengawet</p>
-            </Col>
-          </Row>
+        <h2>Produk Terbaru</h2>
+        <p>Berbagai macam produk sehat dan higienis</p>
+        <Row className='row-cols-lg-3'>
+          <Col sm={12} md={6} lg={4}>
+          <div className='single-product'>
+            <div className="image-container">
+              <Image src={image2} fluid style={{ width: '100%', height: '100%' }}/>
+              <div className="image-title">
+                <h5>Beras organik</h5>
+              </div>
+            </div>
+          </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+          <div className='single-product'>
+            <div className="image-container">
+              <Image src={image3} fluid style={{ width: '100%', height: '100%' }}/>
+              <div className="image-title">
+                <h5>Abon 100% Daging Asli</h5>
+              </div>
+            </div>
+          </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+          <div className='single-product'>
+            <div className="image-container">
+              <Image src={image4} fluid style={{ width: '100%', height: '100%' }}/>
+              <div className="image-title">
+                <h5>Tepung Bumbu Krispi Serbaguna</h5>
+              </div>
+            </div>
+          </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+          <div className='single-product'>
+            <div className="image-container">
+              <Image src={image5} fluid style={{ width: '100%', height: '100%' }}/>
+              <div className="image-title">
+                <h5>Kaldu Jamur</h5>
+              </div>
+            </div>
+          </div>
+          </Col>
+          <Col sm={12} md={6} lg={4}>
+          <div className='single-product'>
+            <div className="image-container">
+              <Image src={image6} fluid style={{ width: '100%', height: '100%' }}/>
+              <div className="image-title">
+                <h5>Bumbu Instan Rempah</h5>
+              </div>
+            </div>
+          </div>
+          </Col>
+        </Row>
       </Container>
     </section>
   )
